e0e0270b14ca9bdc266a2dafec4a13ce2bc4f328
DatTruong0108 <dattantruong.work@gmail.com>
2025-06-25 16:18:24 +0700

    add missed components

---
+++ src/types/models/admin/accountManagement.tsx
@@ -0,0 +1,97 @@
+import { BaseApiResponse } from "@/types/baseApiResponse";
+
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  BLOCKED = 'BLOCKED',
+}
+
+export enum UserRole {
+  CUSTOMER = 'CUSTOMER',
+  ADMIN = 'ADMIN',
+  SYSTEM_ADMIN = 'SYSTEM_ADMIN',
+  ORGANIZER = 'ORGANIZER',
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  role: string;
+  createAt: string;
+  status: UserStatus;
+  avatar_id?: number;
+};
+
+export interface UserAdmin {
+  userId: string;
+  name: string;
+  email: string;
+  status: string;
+  role_id: number;
+  created_at: string;
+  role: {
+    role_name: string;
+  }
+}
+
+export interface UsersData {
+  users: User[];
+  total: number;
+}
+
+export type UsersResponse = BaseApiResponse<UsersData>;
+
+export interface ConfirmActiveProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  currentStatus: string;
+}
+
+export interface SortConfig<T> {
+  key: keyof T;
+  direction: 'asc' | 'desc';
+}
+
+export interface SortIconProps<T> {
+  field: keyof T;
+  sortConfig: SortConfig<T> | null;
+}
+
+//Search 
+export interface SearchBarProps {
+  onSearch: (keyword: string) => void;
+}
+
+export interface AccountTableProps {
+  searchKeyword: string;
+  roleFilter: string;
+  dateFrom: string;
+  dateTo: string;
+}
+
+//Filter
+export interface FilterProps {
+  roleFilter: string;
+  onRoleChange: (value: string) => void;
+  dateFrom: string;
+  dateTo: string;
+  onDateFromChange: (value: string) => void;
+  onDateToChange: (value: string) => void;
+  onReset: () => void;
+}
+
+export interface AccountTableProps {
+  users: User[];
+  totalItems: number;
+  currentPage: number;
+  pageSize: number;
+  onPageChange: (page: number) => void;
+  loading: boolean;
+  searchKeyword: string;
+  roleFilter: string;
+  dateFrom: string;
+  dateTo: string;
+  onStatusUpdate: (updatedUsers: User[]) => void;
+}
