83efaa82eb02b663e3a4121a6cba6da73b5776bb
DatTruong0108 <dattantruong.work@gmail.com>
2025-06-29 22:41:02 +0700

    handle languages

--- src/app/(protected)/admin/account-management/_components/accountPage.tsx
+++ src/app/(protected)/admin/account-management/_components/accountPage.tsx
@@ -44,8 +44,8 @@ export default function AccountPage() {
   return (
     <>
       <>
-        <h1 className="text-2xl font-bold text-[#0C4762] mb-1">Quản lý Account</h1>
-        <p>Quản lý thông tin và trạng thái tài khoản</p>
+        <h1 className="text-2xl font-bold text-[#0C4762] mb-1">{transWithFallback('accountManagement', 'Quản lý Account')}</h1>
+        <p>{transWithFallback('infoAndAccStatusManagement', 'Quản lý thông tin và trạng thái tài khoản')}</p>
         <div className="border-t-2 border-[#0C4762] mt-2"></div>
 
         <div className="flex justify-between items-center mt-6 mb-2">
