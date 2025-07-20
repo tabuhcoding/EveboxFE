1a14a2c11e8f100a2a1a371d77c05dbda7a204c9
21120185 <21120185@student.hcmus.edu.vn>
2025-07-20 23:06:49 +0700

    fix check org permission

--- src/app/(protected)/organizer/create-event/layout.tsx
+++ src/app/(protected)/organizer/create-event/layout.tsx
@@ -1,6 +1,5 @@
 import { getCurrentUser } from "@/services/auth.service";
 import { redirect } from "next/navigation";
-import PermissionDeniedModal from "../../admin/_components/PermissionDeniedModal";
 
 export const metadata = {
   title: 'Create Event',
@@ -19,20 +18,7 @@ export default async function CreateEventLayout({
       console.error('Failed to fetch user info:', err);
       redirect('/login'); // if not logged in or failed
     }
-    const allowedRoles = [1, 2];
-    if (!allowedRoles.includes(user?.role)) {
-        return (
-          <div className="flex min-h-screen flex-col md:flex-row">
-          <div className="flex-1 p-6 bg-gray-100 text-black">
-             <PermissionDeniedModal
-            title="Access Denied"
-            message="You do not have organizer permission."
-            redirectUrl="/"
-          />
-          </div>
-        </div>
-        );
-      }
+
   return (
     children
   );
