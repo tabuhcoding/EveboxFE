01486ca4052a66a683b5ecb8599f1af6e04ab50b
21120185 <21120185@student.hcmus.edu.vn>
2025-07-22 22:44:40 +0700

    fix add duplicate ticket

--- src/app/(protected)/organizer/create-event/[id]/libs/functions/showing/addTicket.tsx
+++ src/app/(protected)/organizer/create-event/[id]/libs/functions/showing/addTicket.tsx
@@ -4,20 +4,21 @@ import { TicketProps } from "../../../libs/interface/dialog.interface";
 import { Showtime } from "../../../libs/interface/idevent.interface";
 
 export const addTicket = (
-    showtimeId: string, 
+    showtimeIndex: number,
     newTicket: TicketProps,
     setShowtimes: React.Dispatch<React.SetStateAction<Showtime[]>>
 ) => {
-    setShowtimes((prevShowtimes) =>
-        prevShowtimes.map((showtime) =>
-            showtime.id === showtimeId
-                ? { 
-                    ...showtime, 
-                    tickets: [...showtime.tickets, newTicket].sort((a, b) => 
-                        parseFloat(a.price) - parseFloat(b.price)
-                    ) 
-                }
-                : showtime
-        )
-    );
+    setShowtimes((prevShowtimes) => {
+        const updatedShowtimes = [...prevShowtimes];
+        const updatedTickets = [...updatedShowtimes[showtimeIndex].tickets, newTicket].sort(
+            (a, b) => parseFloat(a.price) - parseFloat(b.price)
+        );
+
+        updatedShowtimes[showtimeIndex] = {
+            ...updatedShowtimes[showtimeIndex],
+            tickets: updatedTickets,
+        };
+
+        return updatedShowtimes;
+    });
 };
